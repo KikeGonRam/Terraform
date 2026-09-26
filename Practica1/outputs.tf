12ec5e7e3a8b6789c7c01ff3770b32f40a724fbe
KikeGonRam <kikeramirez160418@gmail.com>
2026-09-26 00:10:54 -0600

    eejcucion de las nuevas varibles y output

--- Practica1/outputs.tf
+++ Practica1/outputs.tf
@@ -6,46 +6,22 @@ output "unique" {
   value = local.unique
 }
 
-#variable
-variable "enable_monitoring" {
-    description = "Habilitar monitoreo"
-    type        = bool
-    default     = false
+output "enable_monitoring" {
+  value = var.enable_monitoring
 }
 
-variable "regions" {
-    description = "Lista de regiones"
-    type        = list(string)
-    default     = ["us-east-1", "us-west-2"]
+output "regions" {
+  value = var.regions
 }
 
-variable "environment_tags" {
-    description = "Ambiente de despliegue (dev, prod, etc.)"
-    type        = map(string)
-    default     = {
-        dev  = "Development"
-        prod = "Production"
-    }
+output "environment_tags" {
+  value = var.environment_tags
 }
 
-variable "aplication_config" {
-    description = "Configuración de la aplicación"
-    type        = object({
-        name        = string
-        version     = string
-        description = string
-    })
-    default     = {
-        name        = "MyApp"
-        version     = "1.0.0"
-        dependencies = ["lib1", "lib2"]
-        description = "Aplicación de ejemplo"
-    }
+output "application_config" {
+  value = var.aplication_config
 }
 
-
-variable "allowed_networks" {
-    description = "Redes permitidas"
-    type        = set(string)
-    default     = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+output "allowed_networks" {
+  value = var.allowed_networks
 }
--- Practica1/variables.tf
+++ Practica1/variables.tf
@@ -1,7 +1,49 @@
+variable "enable_monitoring" {
+  description = "Habilitar monitoreo"
+  type        = bool
+  default     = false
+}
+
+variable "regions" {
+  description = "Lista de regiones"
+  type        = list(string)
+  default     = ["us-east-1", "us-west-2"]
+}
+
+variable "environment_tags" {
+  description = "Ambiente de despliegue (dev, prod, etc.)"
+  type        = map(string)
+  default     = {
+    dev  = "Development"
+    prod = "Production"
+  }
+}
+
+variable "aplication_config" {
+  description = "Configuración de la aplicación"
+  type        = object({
+    name        = string
+    version     = string
+    description = string
+  })
+  default     = {
+    name        = "MyApp"
+    version     = "1.0.0"
+    dependencies = ["lib1", "lib2"]
+    description = "Aplicación de ejemplo"
+  }
+}
+
+variable "allowed_networks" {
+  description = "Redes permitidas"
+  type        = set(string)
+  default     = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+}
+
 variable "length" {
   description = "Longitud del string aleatorio"
   type        = number
-  default     = 16
+  default     = 8
 }
 
 variable "application_name" {
