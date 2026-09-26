b0735d98e39ad54829837da08e36b58f08eacb19
KikeGonRam <kikeramirez160418@gmail.com>
2026-09-25 23:20:43 -0600

    Modificar el archico main para la variable

--- Practica1/main.tf
+++ Practica1/main.tf
@@ -1,15 +1,4 @@
-terraform {
-    required_providers {
-        random = {
-            source  = "hashicorp/random"
-            version = "~> 3.5.1 "
-        }
-    }
-
-    required_version = ">= 1.0.0"
-}
-
 resource "random_string" "suffix" {
-    length  = 16
+    length  = var.length
     special = true
 }
